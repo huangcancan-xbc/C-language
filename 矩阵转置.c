e145ffe1cf20ddaf01283cf92e7f9dac74c73a5e
黄灿灿 <hc1960074081@qq.com>
2024-07-17 13:12:17 +0800

    牛客：BC107 矩阵转置

---
+++ 矩阵转置.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//BC107 ¾ØÕó×ªÖÃ
+
+#include <stdio.h>
+
+int main() {
+    int n = 0, m = 0;
+    scanf("%d %d", &n, &m);
+    int a[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d ", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
