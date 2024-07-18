e54534fcd434959e454cf776471bf61ff6daaffe
黄灿灿 <hc1960074081@qq.com>
2024-07-18 19:42:29 +0800

    喝汽水问题

---
+++ 喝汽水问题.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以喝多少汽水（编程实现）。
+#include <stdio.h>
+int main()
+{
+	int money = 20;
+	int kong = 20;
+	int sum = 20;
+	while (kong >= 2)
+	{
+		int new_kong = kong / 2;
+		sum += new_kong;
+		kong = new_kong + kong % 2;
+	}
+	printf("%d", sum);
+}
+
+//
+///*
+//思路：
+//1. 20元首先可以喝20瓶，此时手中有20个空瓶子
+//2. 两个空瓶子可以喝一瓶，喝完之后，空瓶子剩余：empty/2(两个空瓶子换的喝完后产生的瓶子) + empty%2(不够换的瓶子)
+//3. 如果瓶子个数超过1个，可以继续换，即重复2
+//*/
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//
+//
+//	scanf("%d", &money);
+//
+//	//方法1
+//	total = money;
+//	empty = money;
+//	while (empty > 1)
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty % 2;
+//	}
+//
+//	printf("total = %d\n", total);
+//	return 0;
+//}
+//
+//
+//// 方法二：按照上述喝水和用瓶子换的规则的话，可以发现，其实就是个等差数列：money*2-1
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//
+//
+//	scanf("%d", &money);
+//
+//	//方法2
+//	if (money <= 0)
+//	{
+//		total = 0;
+//	}
+//	else
+//	{
+//		total = money * 2 - 1;
+//	}
+//	printf("total = %d\n", total);
+//
+//
+//	return 0;
+//}
